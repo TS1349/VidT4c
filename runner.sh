fd52431a64fe2995476a01e696bf78c05e5568a0
TS1349 <teimurazsaghinadze@gmail.com>
2025-07-28 01:59:40 +0400

    removed extra items

--- runner.sh
+++
@@ -1,8 +0,0 @@
-python -u ./runner.py \
-        --epochs 15\
-        --batch_size 16\
-        --learning_rate 0.005\
-        --weight_decay 0.0004\
-        --csv_file "$SCRATCH"\
-        --checkpoint_dir "./checkpoints"\
-        --experiment_name "first run"
