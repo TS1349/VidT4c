7b843ea4c38a2e924da1e2fd026fb0556d9d0288
TS1349 <teimurazsaghinadze@gmail.com>
2025-08-14 02:31:51 +0400

    changed to uniform samling routine issues pending: - remapping of v a happens between values of 1-8, but there's 9   sometimes - EAV-s self_annotation is "sadness" etc.

--- dataloader/ve_dataset.py
+++ dataloader/ve_dataset.py
@@ -5,29 +5,27 @@ import pandas as pd
 import math
 from .utils import torch_random_int
 
-class VERandomDataset(Dataset):
+class VEUniformDataset(Dataset):
     def __init__(
             self,
             csv_file,
             eeg_sampling_rate,
             eeg_channel_count,
             output_shape,
-            time_window,
             split="train",
             video_output_format = "TCHW",
             video_transform = None,
             eeg_transform = None,
             num_out_frames = 32,
             num_out_eeg = 64,
+            ignore_list = None,
             ):
         
         self.csv_file = str(csv_file)
         self.split = split
         self.video_output_format = video_output_format
 
-        self.time_window = time_window
         self.num_out_frames = num_out_frames
-        self.time_sub_window = time_window / (num_out_frames - 1)
 
 
         self.eeg_sampling_rate = eeg_sampling_rate
@@ -42,22 +40,16 @@ class VERandomDataset(Dataset):
 
         df = pd.read_csv(self.csv_file)
         self.df = df[(df["data_split"] == self.split) & (df["bool_both_file"] == True)]
-        sample_row = self.df.iloc[0]
-        anno_type = sample_row.anno_type
 
-        if anno_type == "category":
-            self._get_label = self._get_ctgr_labels
-        else:
-            self._get_label = self._get_cont_labels
-
+        if ignore_list is not None:
+            regex_string = f"subject{\
+                str(ignore_list).replace(" ", "").replace(",", "|")}"
+            self.df = self.df[~df.facial_video.str.contains(regex_string, regex = True)]
 
     def __len__(self):
         return len(self.df)
 
-    def _get_label(self, *args):
-        raise NotImplementedError("This fuction shouldn't have been accessed")
-
-    def _get_cont_labels(self, row):
+    def _get_label(self, row):
         # returns 3 int32 class ids starting from 0
         self_annotation = row.self_annotation[1:-1].split(r",")
         self_annotation = [ int(entry) - 1 for entry in self_annotation ]
@@ -72,55 +64,35 @@ class VERandomDataset(Dataset):
         }
         va_labels = [mapping[val] for val in self_annotation[:2]]  # Only valence, arousal
         return torch.tensor(va_labels, dtype=torch.int64)
-
-    def _get_ctgr_labels(self, row):
-        return torch.tensor(row.label_id, dtype = torch.int64)
     
     def _get_full_eeg(self, row):
         eeg = pd.read_csv(row.EEG) # Sample x channel
         eeg = torch.tensor(eeg.to_numpy(), dtype = torch.float32)
+        #primarly for Emognition, but makes sense to run it for all of them
+        eeg = torch.nan_to_num(eeg)
         return eeg
     
-    @staticmethod
-    def _decimate_idxs(start_idx, end_idx, final_number):
-        delta = end_idx - start_idx
-        return [ round(start_idx + i * delta / (final_number -1)) for i in range(final_number) ]
-    
-    def _get_random_frame_idxs(self, total_frames, fps):
-        # num_frames_in_window = math.floor(fps * self.time_window)
 
-        # lower_bound = math.ceil((num_frames_in_window / (self.num_out_frames - 1)))
-        # upper_bound = total_frames - num_frames_in_window - lower_bound - 1 # since num_frames_in_window is floored
+    def _get_unform_frame(self, fps, video, eeg):
+        eeg_sample_count = eeg.shape[0]
 
-        # random_start_idx = torch_random_int(
-        #     low = lower_bound,
-        #     high = upper_bound, # ! low + 32 x 6.4
-        #     )
-        
-        # idxs = VERandomDataset._decimate_idxs(
-        #     start_idx = random_start_idx,
-        #     end_idx = random_start_idx + num_frames_in_window,
-        #     final_number=self.num_out_frames)
+        video_time = video.shape[0] / fps 
+        eeg_time = eeg_sample_count / self.eeg_sampling_rate
 
-        idxs = VERandomDataset._decimate_idxs(
-            start_idx = 5,
-            end_idx = total_frames - 5,
-            final_number=self.num_out_frames)        
+        if (eeg_time > video_time):
+            eeg_sample_count = round(video_time * self.eeg_sampling_rate)
 
-        return idxs
+        #take multiple of video frames
+        eeg_sample_step = eeg_sample_count // self.num_out_frames
+        eeg_upper_idx = self.num_out_frames * eeg_sample_step + 1
+
+        video_idxs =[
+            round((i + 0.5)*eeg_sample_step / self.eeg_sample_rate*fps) for i in range(self.num_out_frames)]
+
+        return (video[video_idxs,...], eeg[:eeg_upper_idx, ...])
+
     
     def _get_corresponding_eeg_idxs(self, frame_idxs, fps):
-        # num_samples_sub_window = round(self.time_sub_window * self.eeg_sampling_rate)
-
-        # eeg_idxs = [ math.floor((idx  * self.eeg_sampling_rate / fps)) - num_samples_sub_window // 2
-        #             for idx in frame_idxs ]
-        # eeg_idxs.append(eeg_idxs[-1] + num_samples_sub_window)
-
-        # decimated_eeg_idxs_list = [ VERandomDataset._decimate_idxs(
-        #     start_idx = eeg_idxs[i],
-        #     end_idx = eeg_idxs[i+1],
-        #     final_number=self.num_out_eeg
-        #     ) for i in range(len(eeg_idxs)-1) ]
         
         start_eeg_idx = math.floor(frame_idxs[0] * self.eeg_sampling_rate / fps)
         end_eeg_idx = math.floor(frame_idxs[-1] * self.eeg_sampling_rate / fps)
@@ -134,39 +106,20 @@ class VERandomDataset(Dataset):
         row = self.df.iloc[idx]
         video_path = row.facial_video
 
-        if any(f"EAV/subject{sid}" in video_path for sid in [4, 5, 18, 20, 38]):
-            return self.__getitem__((idx + 1) % len(self.df))      
-
-        try:
-            video, _, metadata = read_video(
-                filename=video_path,
-                pts_unit="sec",
-                output_format=self.video_output_format,
+        video, _, metadata = read_video(
+            filename=video_path,
+            pts_unit="sec",
+            output_format=self.video_output_format,
             )
-        except (RuntimeError, FileNotFoundError) as e:
-            print(f"Skipping file {video_path} because: {e}")
-            return self.__getitem__((idx + 1) % len(self.df))
         
         fps = metadata["video_fps"]
-
         eeg = self._get_full_eeg(row)
-        eeg_time_to_frames = math.floor(math.floor((eeg.shape[0] / self.eeg_sampling_rate)) * fps) # Seconds x fps for EEG frame
 
-        total_frames = min(video.shape[0], eeg_time_to_frames) # Based on frame
-
-        video_idxs = self._get_random_frame_idxs(total_frames, fps)
-        video = video[video_idxs, ...]
-
+        video,eeg = self._get_uniform_frames(fps, video, eeg)
 
         if self.video_transform is not None:
             # video = self.video_transform(video)
             video = torch.stack([self.video_transform(frame) for frame in video])
-        
-        eeg_idxs = self._get_corresponding_eeg_idxs(video_idxs, fps)
-        eeg = eeg[eeg_idxs,...]
-
-        if "Emognition" in self.csv_file: # To handle nan value of the eeg on the Emognition dataset.
-            eeg = torch.nan_to_num(eeg)
         
         if self.eeg_transform is not None:
             eeg = self.eeg_transform(eeg)
@@ -180,23 +133,22 @@ class VERandomDataset(Dataset):
                }
 
 
-class EAVDataset(VERandomDataset):
+class EAVDataset(VEUniformDataset):
     def __init__(
             self,
             csv_file,
-            time_window = 15.0,
             split="train",
             video_output_format = "TCHW",
             video_transform = None,
             eeg_transform = None,
             num_out_frames = 32,
             num_out_eeg = 64,
+            ignore_list = [4, 5, 18, 20, 38],
             ):
 
         super(EAVDataset,self).__init__(
                 csv_file = csv_file,
                 eeg_sampling_rate = 500,
-                time_window = time_window,
                 split=split,
                 video_output_format = video_output_format,
                 video_transform = video_transform,
@@ -205,13 +157,13 @@ class EAVDataset(VERandomDataset):
                 num_out_eeg = num_out_eeg,
                 output_shape = (5,1),
                 eeg_channel_count = 30,
+                ignore_list = ignore_list,
         )
 
-class MDMERDataset(VERandomDataset):
+class MDMERDataset(VEUniformDataset):
     def __init__(
             self,
             csv_file,
-            time_window = 15.0,
             split="train",
             video_output_format = "TCHW",
             video_transform = None,
@@ -223,7 +175,6 @@ class MDMERDataset(VERandomDataset):
         super(MDMERDataset,self).__init__(
                 csv_file = csv_file,
                 eeg_sampling_rate = 300,
-                time_window = time_window,
                 split=split,
                 video_output_format = video_output_format,
                 video_transform = video_transform,
@@ -234,11 +185,10 @@ class MDMERDataset(VERandomDataset):
                 eeg_channel_count = 18,
         )
 
-class EmognitionDataset(VERandomDataset):
+class EmognitionDataset(VEUniformDataset):
     def __init__(
             self,
             csv_file,
-            time_window = 30.0,
             split="train",
             video_output_format = "TCHW",
             video_transform = None,
@@ -250,7 +200,6 @@ class EmognitionDataset(VERandomDataset):
         super(EmognitionDataset,self).__init__(
                 csv_file = csv_file,
                 eeg_sampling_rate = 256,
-                time_window = time_window,
                 split=split,
                 video_output_format = video_output_format,
                 video_transform = video_transform,
